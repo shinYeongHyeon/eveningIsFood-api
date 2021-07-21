23a839cdc4b9f0c38982780147a862eae47f6092
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-07-22 01:13:46 +0900

    Add TestCase

--- src/user/domain/UserName.go
+++ src/user/domain/UserName.go
@@ -1,6 +1,9 @@
 package user_domain
 
-import "errors"
+import (
+	"errors"
+	"unicode/utf8"
+)
 
 // UserName structure
 type UserName struct {
@@ -11,6 +14,10 @@ type UserName struct {
 func UserNameCreate(userNameString string) (UserName, error) {
 	if userNameString == "" {
 		return UserName{}, errors.New("userNameString Can Not Be Empty")
+	}
+
+	if utf8.RuneCountInString(userNameString) < 3 {
+		return UserName{}, errors.New("userNameString Length Can Not Be Less Than 3")
 	}
 
 	return UserName { userNameString }, nil
--- src/user/domain/UserName_test.go
+++ src/user/domain/UserName_test.go
@@ -21,3 +21,15 @@ func TestFailToCreateUserNameWhenUserNameStringIsEmpty(t *testing.T) {
 		t.Fatal("Fail to Create UserName")
 	}
 }
+
+// TestFailToCreateUserNameUserNameLessThanThreeWord Testing for UserName Create Fail When userNameString length less than three word.
+func TestFailToCreateUserNameUserNameLessThanThreeWord(t *testing.T) {
+	userNameString := "ab"
+	userNameStringKoreanVer := "신영"
+	_, err := UserNameCreate(userNameString)
+	_, errKoreanVer := UserNameCreate(userNameStringKoreanVer)
+
+	if err == nil || errKoreanVer == nil {
+		t.Fatal("Fail to Create UserName")
+	}
+}
