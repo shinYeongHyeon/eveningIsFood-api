4db1f5c4c4ec4e1822fcfe9d28febb3dbc27c1a1
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-08-11 00:39:52 +0900

    CreateUserUseCase Structure

---
+++ src/user/application/CreateUserUseCase/CreateUserUseCase_test.go
@@ -0,0 +1,31 @@
+package createuserusecase
+import (
+	createuserusecasedto "github.com/shinYeongHyeon/eveningIsFood/eveningIsFoodApi/src/user/application/CreateUserUseCase/dto"
+	"testing"
+)
+
+// TestFailToCreateUserUseCase Testing for CreateUserUseCase Fail
+func TestFailToCreateUserUseCase(t *testing.T) {
+	isSuccess, err := Exec(createuserusecasedto.Request{
+		Email: "",
+		Name: "",
+		Password: "",
+	})
+
+	if isSuccess || err == nil {
+		t.Fatal("Fail to TestFailToCreateUserUseCase")
+	}
+}
+
+// TestCreateUserUseCase Testing for CreateUserUseCase Success
+func TestCreateUserUseCase(t *testing.T) {
+	isSuccess, err := Exec(createuserusecasedto.Request{
+		Email: "den.shin.dev@gmail.com",
+		Name: "신영현",
+		Password: "비밀번호123",
+	})
+
+	if !isSuccess || err != nil {
+		t.Fatal("Fail to TestCreateUserUseCase")
+	}
+}
