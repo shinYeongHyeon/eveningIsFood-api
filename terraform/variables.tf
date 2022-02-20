30513947c6ede5bf77ca580e488420ec3b8d2ca5
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-02-20 15:00:48 +0900

    Init Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  default     = ""
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "region" {
+  default     = "ap-northeast-2"
+  description = "The AWS region to use."
+  type        = string
+}
