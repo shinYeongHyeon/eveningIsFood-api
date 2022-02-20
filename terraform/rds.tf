b2cceda46a05aab8ed5a224296df0ec7e861f281
Steve Kang <iam@juneyoung.io>
2022-02-20 10:46:41 +0000

    FIX: (NONE) Edit rds engine from aurora to mysql
    Co-authored-by: Steve Kang <iam@juneyoung.io>
    
    Merge-request: EIF-MR-4
    Merged-by: Den Shin <den.shin.dev@gmail.com>

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,13 +1,12 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "aurora-mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
+  allocated_storage    = 100
+  engine               = "mysql"
+  engine_version       = "8.0.28"
+  instance_class       = "db.t3.medium"
   db_name              = "eif"
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   apply_immediately    = true
-  character_set_name   = "utf8mb4"
-  timezone             = "KST"
+  publicly_accessible  = true
 }
